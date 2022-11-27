c327ebd309df6d673ca55438ecd9970b998e1bd2
Clément Besnier <clemsciences@aol.com>
2022-11-27 23:06:33 +0100

    Added setup.py, requirements.txt and MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+import io
+from setuptools import setup
+
+
+CURDIR = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(CURDIR, "README.md"), "r", encoding="utf-8") as f:
+    README = f.read()
+
+
+setup(
+    name='sat-biblio-referencement',
+    version='1.0.0',
+    packages=['sat-biblio-referencement'],
+    url='https://github.com/clemsciences/sat-biblio-referencement',
+    license='',
+    author='clemsciences',
+    author_email='clem@clementbesnier.fr',
+    description='SAT BIBLIO Référencement',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=["lxml", "SQLAlchemy", "requests"],
+    python_requires=">=3.6",
+    keywords=['library', 'french'],
+    script=[]
+)
