77bc471344ae0f875ce34a4687429498867a08f6
Clément Besnier <clemsciences@aol.com>
2022-11-27 23:14:50 +0100

    Updated sat_biblio_referencement name

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with io.open(os.path.join(CURDIR, "README.md"), "r", encoding="utf-8") as f:
 
 
 setup(
-    name='sat-biblio-referencement',
+    name='sat_biblio_referencement',
     version='1.0.0',
     packages=['sat_biblio_referencement'],
     url='https://github.com/clemsciences/sat-biblio-referencement',
