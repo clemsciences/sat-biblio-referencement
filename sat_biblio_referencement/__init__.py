792e1eb8a690a23cdfa8eff13ecfebb30672c621
Clément Besnier <clemsciences@aol.com>
2022-12-03 22:24:51 +0100

    Set SQLAlchemy==1.3.22

--- sat_biblio_referencement/__init__.py
+++ sat_biblio_referencement/__init__.py
@@ -3,7 +3,7 @@ import os
 PACKDIR = os.path.abspath(os.path.dirname(__file__))
 
 
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
